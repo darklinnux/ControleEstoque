697252b52080ef7a5b7696b22635d15e78c4fd9c
Pedro Ramon Rodrigues Lima <pedro.ramon@sinobras.com.br>
2023-07-18 16:51:41 -0300

    Criação das rotas da API e estruturação de pastas do projeto

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/v1")
+def getUsuaruis():
+    return {"Version": "0.0.1"}
+
+###ROTAS USUÁRIOS###
+@app.get("/v1/users")
+def getUsuaruis():
+    return {"Version": "0.0.1"}
+
+###ROTAS NOTEBOOKS###
+@app.get("/v1/notebooks")
+def getUsuaruis():
+    return {"Version": "0.0.1"}
+
